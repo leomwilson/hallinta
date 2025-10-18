4d688de3e960668474d0838fbf82057257d00eb5
Leo Wilson <leo@lwilson.dev>
2025-10-17 22:12:39 -0700

    Make things pretty ish

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
+  @Output() addTask = new EventEmitter<void>();
+
+  emitAdd() {
+    this.addTask.emit();
+  }
 
 }
